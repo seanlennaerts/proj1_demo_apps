657983f1a4a7dfd82d9d3e04316008d7d2147dc0
Sean Lennaerts <seanlenny@gmail.com>
2018-02-20 15:35:58 -0800

    Script now pulls the public ips from the correct file depending on environment used

--- bashscript/main.sh
+++ bashscript/main.sh
@@ -1,3 +1,36 @@
 #!/usr/bin/env bash
 
-echo hi
+if [ "$1" == "p" ]; then
+  echo "using P environment"
+  environment="p"
+  
+  # Now read the public ips for P
+  filename="pips.pub"
+  while read -r line
+  do
+    pip="$line"
+    
+    # TODO: Use pip to ssh
+    echo "$pip"
+    
+  done < "$filename"
+
+elif [ "$1" == "q" ]; then
+  echo "using Q environment"
+  environment="q"
+
+  # Now read the public ips for Q
+  filename="qips.pub"
+  while read -r line
+  do
+    qip="$line"
+
+    # TODO: Use qip to ssh
+    echo "$qip"
+
+  done < "$filename"
+
+else 
+  echo "Usage [main.sh] [p or q] "
+fi
+
